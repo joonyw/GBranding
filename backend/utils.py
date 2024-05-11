5cffbcc13328b15295eefc55a0e15db7b45cfe82
Joonwoo Lee <joonyw.lee@gmail.com>
2024-05-11 21:21:14 +0900

    minor clean up

--- backend/utils.py
+++ backend/utils.py
@@ -2,7 +2,8 @@ import openai
 import os
 import urllib
 from pathlib import Path
-import openai
+from moviepy.editor import ImageSequenceClip, AudioFileClip, CompositeVideoClip
+
 
 OPEN_API_KEY='sk-'
 def create_scenario(usr_input):
@@ -19,19 +20,19 @@ def create_scenario(usr_input):
 
 
 def get_pic(resp):
-    # from openai import OpenAI
-    # client = openai.OpenAI(api_key = OPEN_API_KEY,)
+    from openai import OpenAI
+    client = openai.OpenAI(api_key = OPEN_API_KEY,)
 
-    # response = client.images.generate(
-    #     model="dall-e-2",
-    #     prompt="a picture of the following scenarion: " + resp,
-    #     size="1024x1024",
-    #     quality="standard",
-    #     n=1,
-    # )
+    response = client.images.generate(
+        model="dall-e-2",
+        prompt="a picture of the following scenarion: " + resp,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
 
-    # image_url = response.data[0].url
-    # urllib.request.urlretrieve(image_url, "generated_image.jpeg")
+    image_url = response.data[0].url
+    urllib.request.urlretrieve(image_url, "generated_image.jpeg")
     return
 
 def speech(usr_input):
@@ -43,3 +44,15 @@ def speech(usr_input):
     input=usr_input
     )
     response.stream_to_file(speech_file_path)
+
+def make_video(image_folder, audio_file, output_video, fps=0.5):
+    image_files = [os.path.join(image_folder, img) for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
+    clip = ImageSequenceClip(image_files, fps=fps)
+    audio = AudioFileClip(audio_file)
+    video = clip.set_audio(audio)
+
+    video = video.set_duration(audio.duration)
+
+    video.write_videofile("output.mp4", fps=fps, audio_codec="aac", audio_bitrate="192k")
+
+    
